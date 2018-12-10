50f235bc19e8c1c3b167216f2718906119fe6c35
bryan.zhang <yuancheng.zhang@thebeastshop.com>
2018-12-10 18:55:51 +0800

    submit

--- beast-tx-test/beast-tx-test-springcloud-consumer/src/main/java/com/thebeastshop/tx/test/sc/consumer/config/BeanConfiguration.java
+++ beast-tx-test/beast-tx-test-springcloud-consumer/src/main/java/com/thebeastshop/tx/test/sc/consumer/config/BeanConfiguration.java
@@ -3,6 +3,8 @@ package com.thebeastshop.tx.test.sc.consumer.config;
 import com.thebeastshop.tx.aop.TxAspect;
 import com.thebeastshop.tx.feign.aop.FeignTxAspect;
 import com.thebeastshop.tx.feign.spring.FeignMethodScanner;
+import com.thebeastshop.tx.socket.client.SocketClient;
+import com.thebeastshop.tx.socket.factory.SocketClientFactory;
 import feign.Request;
 import feign.Retryer;
 import org.springframework.beans.factory.annotation.Value;
@@ -46,4 +48,13 @@ public class BeanConfiguration {
     public FeignTxAspect feignTxAspect(){
         return new FeignTxAspect();
     }
+
+    @Bean
+    public SocketClientFactory socketClient() throws Exception {
+        SocketClientFactory factory=  new SocketClientFactory();
+        factory.setIp("127.0.0.1");
+        factory.setPort(6789);
+        return factory;
+    }
+
 }
