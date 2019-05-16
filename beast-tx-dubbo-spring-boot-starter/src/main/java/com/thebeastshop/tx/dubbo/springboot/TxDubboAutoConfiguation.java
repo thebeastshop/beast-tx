b46b169f70044f4baf1b73ca779cd89e4fd33589
bryan.zhang <yuancheng.zhang@thebeastshop.com>
2019-05-16 13:32:15 +0800

    dubbo tx的自动装配优化

--- beast-tx-dubbo-spring-boot-starter/src/main/java/com/thebeastshop/tx/dubbo/springboot/TxDubboAutoConfiguation.java
+++ beast-tx-dubbo-spring-boot-starter/src/main/java/com/thebeastshop/tx/dubbo/springboot/TxDubboAutoConfiguation.java
@@ -11,11 +11,18 @@ import com.thebeastshop.tx.aop.TxAspect;
 import com.thebeastshop.tx.dubbo.spring.DubboMethodScanner;
 import com.thebeastshop.tx.socket.client.SocketClient;
 import com.thebeastshop.tx.socket.factory.SocketClientFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
+import org.springframework.lang.Nullable;
+
+import javax.annotation.Resource;
 
 /**
  * tx dubbo的自动装配
@@ -23,44 +30,26 @@ import org.springframework.core.annotation.Order;
 @Configuration
 public class TxDubboAutoConfiguation {
 
-    @Value("${tx.monitor.ip}")
-    private String txMonitorIp;
+    private final String TX_MONITOR_IP = "tx.monitor.ip";
 
-    @Value("${tx.monitor.port}")
-    private int txMonitorPort;
+    private final String TX_MONITOR_PORT = "tx.monitor.port";
 
     @Bean
-    @ConditionalOnProperty(prefix = "tx.monitor",name = "ip")
-    public SocketClientFactory socketClientFactory(){
+    @ConditionalOnProperty(prefix = "tx.monitor",name = {"ip","port"})
+    public SocketClientFactory socketClientFactory(Environment environment){
         SocketClientFactory socketClientFactory = new SocketClientFactory();
-        socketClientFactory.setIp(txMonitorIp);
-        socketClientFactory.setPort(txMonitorPort);
+        socketClientFactory.setIp(environment.getProperty(TX_MONITOR_IP));
+        socketClientFactory.setPort(environment.getProperty(TX_MONITOR_PORT,Integer.class));
         return socketClientFactory;
     }
 
     @Bean
-    public TxAspect txAspect(){
+    public TxAspect txAspect(@Nullable SocketClient socketClient){
         return new TxAspect();
     }
 
     @Bean
     public DubboMethodScanner dubboMethodScanner(){
         return new DubboMethodScanner();
-    }
-
-    public String getTxMonitorIp() {
-        return txMonitorIp;
-    }
-
-    public void setTxMonitorIp(String txMonitorIp) {
-        this.txMonitorIp = txMonitorIp;
-    }
-
-    public int getTxMonitorPort() {
-        return txMonitorPort;
-    }
-
-    public void setTxMonitorPort(int txMonitorPort) {
-        this.txMonitorPort = txMonitorPort;
     }
 }
