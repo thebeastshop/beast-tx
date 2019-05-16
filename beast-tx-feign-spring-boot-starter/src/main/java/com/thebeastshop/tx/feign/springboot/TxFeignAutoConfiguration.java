e951d680dc4f86281b9698bd1e99a39f199a2082
bryan.zhang <yuancheng.zhang@thebeastshop.com>
2019-05-16 23:19:02 +0800

    优化monitor

--- beast-tx-feign-spring-boot-starter/src/main/java/com/thebeastshop/tx/feign/springboot/TxFeignAutoConfiguration.java
+++ beast-tx-feign-spring-boot-starter/src/main/java/com/thebeastshop/tx/feign/springboot/TxFeignAutoConfiguration.java
@@ -1,3 +1,10 @@
+/**
+ * <p>Title: beast-tx</p>
+ * <p>Description: 分布式事务框架，基于TCC事务的事务框架监控跟踪平台</p>
+ * @author Bryan.Zhang
+ * @email weenyc31@163.com
+ * @Date 2019/5/16
+ */
 package com.thebeastshop.tx.feign.springboot;
 
 import com.thebeastshop.tx.aop.TxAspect;
